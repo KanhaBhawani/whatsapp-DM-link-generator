6308dc9b5afe696e30fdeba66b9efd48db31a027
Kanha Bhawani <ghanishtsinghal@gmail.com>
2022-06-26 15:46:31 +0530

    completing backend

--- server.js
+++ server.js
@@ -5,18 +5,15 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = 3000;
-const hostname = '0.0.0.0';
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", function (req, res) {
-    var phoneNo = req.body.phone;
-    res.send('<a href="https://wa.me/+91' + phoneNo + '">click here</a>');
-    // res.send(phoneNo);
+    res.sendFile(__dirname + "/link.html")
 });
 
-app.listen(port, hostname, function () {
-    console.log('server running at http://'+hostname+':'+port+'/');
+app.listen(port, function () {
+    console.log('server running at localhost:3000');
 })
